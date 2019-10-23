20c55c10ddb3eede58a92648895bf0bbf979565e
DEVINccp <1255618958@qq.com>
2019-10-23 15:14:04 +0800

    第四次提交

--- src/AlgorithmLearn.java
+++ src/AlgorithmLearn.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-
 public class AlgorithmLearn {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        solution sol = new solution();
 //        ****************1****************
 //        Scanner input=new Scanner(System.in);
 //        int target=input.nextInt();
@@ -20,9 +19,25 @@ public class AlgorithmLearn {
 
 //    ****************2******************
 //    将空格替换为%20
-        Scanner input=new Scanner(System.in);
-        String s=input.nextLine();
-        System.out.println(solution.replaceSpace(s));
-    }
+//        Scanner input=new Scanner(System.in);
+//        String s=input.nextLine();
+//        System.out.println(solution.replaceSpace(s));
+//    }
 
+//    *********************7*********************
+//    现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+//    n<=39
+//        Scanner input = new Scanner(System.in);
+//        Integer tem = 0;
+//        tem = input.nextInt();
+//        System.out.println(sol.Fibonacci(tem));
+
+//    *********************8**********************
+//    一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上
+//    一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）
+        Scanner input=new Scanner(System.in);
+        Integer tem =0;
+        tem=input.nextInt();
+        System.out.println(sol.JumpFloor1(tem));
+    }
 }
--- src/solution.java
+++ src/solution.java
@@ -174,4 +174,43 @@ public class solution {
         }
         return 0;
     }
+
+//    *********************7*********************
+//    现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+//    n<=39
+    public int Fibonacci(int n) {
+        if(n<=1)
+        {
+            return n;
+        }
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+
+//    *********************8**********************
+//    一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上
+//    一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）
+//     递归解法
+    public int JumpFloor(int target) {
+        if(target==1) return 1;
+        if(target==2) return 2;
+        if(target==3) return 3;
+        return JumpFloor(target-1)+JumpFloor(target-2);
+    }
+
+    //迭代解法
+    public int JumpFloor1(int target) {
+        int pre1=2;
+        int pre2=1;
+        if(target==1) return 1;
+        if(target==2) return 2;
+        int re=0;
+        for(int i=3;i<=target;i++)
+        {
+            re=pre1+pre2;
+            pre2=pre1;
+            pre1=re;
+        }
+        return re;
+    }
+
 }
